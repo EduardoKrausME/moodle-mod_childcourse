280450595d24c0d7a77ed34a7967f515b0bd77aa
Eduardo Kraus <kraus@eduardokraus.com>
2026-04-08 14:55:14 -0300

    V 2.0.2

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026040801;
-$plugin->release = "2.0.1";
+$plugin->version = 2026040802;
+$plugin->release = "2.0.2";
 $plugin->component = "mod_childcourse";
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
